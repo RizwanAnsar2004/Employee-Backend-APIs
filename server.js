d5b56a4670c8de8f594ac5bbd6012ce479e8a9d7
Rizwan Ansar <ansarrizwan83@gmail.com>
2025-07-01 02:26:33 +0500

    remove redundant db calls

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const dotenv = require('dotenv');
 dotenv.config(); 
 
-console.log('DEBUG: MONGO_URI is', process.env.MONGO_URI); 
+// console.log('DEBUG: MONGO_URI is', process.env.MONGO_URI); 
 
-const dbConnect = require('./Config/db');
+
 const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
 
-dbConnect();
+// dbConnect();
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
